c261d35ec2f5bf6ea3050365fcf8a72d171accc5
Tomasz.Ancukiewicz <tomasz.ancukiewicz@payu.com>
2020-02-18 12:59:37 +0100

    git init

---
+++ spec/story-points-count-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StoryPointsCountView from '../lib/story-points-count-view';
+
+describe('StoryPointsCountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
