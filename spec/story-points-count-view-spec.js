b39bfd1ba39d2570ddc04eb73aed918e7c2afc0d
Tomasz.Ancukiewicz <tomasz.ancukiewicz@payu.com>
2021-02-17 14:52:39 +0100

    add grammars and remove unused files

--- spec/story-points-count-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import StoryPointsCountView from '../lib/story-points-count-view';
-
-describe('StoryPointsCountView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
